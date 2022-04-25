808ec25f228bbda185ea8a6cf1d50ca20b785588
Sparkeagle2000 <43283207+Sparkeagle2000@users.noreply.github.com>
2022-04-25 15:35:01 -0400

    Update Movement.cs
    did it

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,8 @@ public class Movement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float jump,speed;
+    Vector3 currentEulerAngles;
+    Quaternion currentRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,17 @@ public class Movement : MonoBehaviour
     {
         speed=Input.GetAxisRaw("Horizontal");
         rb.velocity=new Vector2(speed*7f,rb.velocity.y);
+        if(rb.velocity.x<0)
+        {
+            currentEulerAngles = new Vector3(0, 180, 0);
+            currentRotation.eulerAngles = currentEulerAngles;
+            transform.rotation = currentRotation;
+        }
+        else{
+            currentEulerAngles = new Vector3(0, 0, 0);
+            currentRotation.eulerAngles = currentEulerAngles;
+            transform.rotation = currentRotation;
+        }
         if(Input.GetButtonDown("Jump"))
         {
             rb.velocity=new Vector2(rb.velocity.x,jump);
