47262daccab8cc0da94b82326d42538d4c15c150
Sparkeagle2000 <43283207+Sparkeagle2000@users.noreply.github.com>
2022-04-25 14:43:25 -0400

    Camera working well
    adjusted the camera to where I think we want it

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,33 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class Camera : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class CameraController : MonoBehaviour {
+
+public GameObject player;        //Public variable to store a reference to the player game object
+public bool followY = false;
+
+private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+
+// Use this for initialization
+    void Start () 
     {
-        
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+        offset = new Vector3(offset.x,0,offset.z);
     }
 
-    // Update is called once per frame
-    void Update()
+// LateUpdate is called after Update each frame
+    void LateUpdate () 
     {
-        
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        if(followY)
+          {
+          transform.position = player.transform.position + offset; // we should follow the player Y movements, so we copy the entire position vector
+          }
+        else
+          {
+          transform.position = new Vector3(player.transform.position.x, 0.34f, player.transform.position.z) + offset; // we just copy the X and Z values
+          }
     }
 }
